5b6838c14c14ef79cfc31b495245454524a20cd6
Eric Ehmann <eaehmann@gmail.com>
2016-09-19 14:18:02 -0400

    changed index to hopefully show errors

--- public/index.php
+++ public/index.php
@@ -26,6 +26,6 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 			<input type="radio" name="sym" value="Yes"> Yes <br>
 			<input type="submit" value="Generate">
 		</form>
-        <? php echo $pass ?>
+        
 
 	</body>	
