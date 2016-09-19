39e124ed6de9a0bd5007cbe1017b04507a63c100
Eric Ehmann <eaehmann@gmail.com>
2016-09-19 17:58:12 -0400

    hardcoding passwordLength to troubleshoot

--- public/logic.php
+++ public/logic.php
@@ -1,5 +1,5 @@
 <?php
-	$pass = "Please enter a number between 1 and 6 in numeric form";
+	$pass = "bye";
 	$wordList =[
 	'marathon',
 	'soccer',
@@ -12,28 +12,32 @@
 	'cook',
 	'minute',
 	'line'];
-	$passwordLength = (int)( $_POST["length"]);
-	$wantNumber = ( $_POST["num"]);
-	$wantSymbol = ( $_POST["sym"]);
+	$passwordLength =5;
+	//$passwordLength = (int)( $_POST["length"]);
+	//$wantNumber = ( $_POST["num"]);
+	//$wantSymbol = ( $_POST["sym"]);
 	if($passwordLength >=1 && $passwordLength <=6){
 		#replace default string with one word
-		$pass = $wordList[random_int(0, 10)];
+		$pass = $wordList[rand(0, 10)];
 		#If the user wants to use a number this will randomly select whaere it goes
-		if($wantNumber == "Yes"){
-			$numPlace =random_int(1, ($passwordLength-1));
-		}
+		//if($wantNumber == "Yes"){
+		//	$numPlace =rand(1, ($passwordLength-1));
+		//}
 		#If the user wants a symbol randomly choose where it should go
-		if($wantSymbol=="Yes"){
-		$symPlace = random_int (1, ($passwordLength-1));
-		}
+		//if($wantSymbol=="Yes"){
+		//$symPlace = rand(1, ($passwordLength-1));
+		//}
 		#loop that checks if it is the right place to add a number then checks if it's the right place to add a symbol and then adds a word
 		for($i =1; $i <$passwordLength; $i++){
-			if($i == $numplace){
-				$pass = $pass + random_int(1,99);
-			}
-			if ($i == $symPlace){
-				$pass= $pass + char(random_int(33, 47));
-			}
-			$pass = $pass + $wordList[random_int(0, 10)];
+			//if($i == $numplace){
+			//	$pass = $pass + rand(1,99);
+			//}
+			//if ($i == $symPlace){
+			//	$pass= $pass + char(rand(33, 47));
+			//}
+			$pass = $pass + $wordList[rand(0, 10)];
 		}
+	}
+	else{
+		$pass ="Please write the number of words in text box above";
 	}
