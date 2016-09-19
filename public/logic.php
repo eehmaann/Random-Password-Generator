e799ee28791f638ec044754550f35344ae8ef1ad
Eric Ehmann <eaehmann@gmail.com>
2016-09-19 12:13:01 -0400

    still working on error

--- public/logic.php
+++ public/logic.php
@@ -12,31 +12,31 @@
 	'cook',
 	'minute',
 	'line'];
-	$passwordLength = parseInt( $_POST["length"]);
-	$wantNumber = ( $_POST["num"]);
-	$wantSymbol = ( $_POST["sym"]);
-	if($passwordLength >=1 AND <=6){
+	//$passwordLength = parseInt( $_POST["length"]);
+	//$wantNumber = ( $_POST["num"]);
+	//$wantSymbol = ( $_POST["sym"]);
+	//if($passwordLength >=1 AND <=6){
 		//replace default stong with one word
-		$pass = $wordList[random_int(0, 10)]
+		//$pass = $wordList[random_int(0, 10)]
 		# If the user wants to use a number this will randomly select whaere it goes
-		if($wantNumber = "Yes"){
-			$numPlace =random_int(1, ($passwordLength-1));
-		}
+		#if($wantNumber = "Yes"){
+			//$numPlace =random_int(1, ($passwordLength-1));
+		//}
 		#If the user wants a symbol randomly choose where it should go
-		if($wantSymbol="Yes"){
-			$symPlace = random_int (1, ($passwordLength-1));
-		}
+		//if($wantSymbol="Yes"){
+		//	$symPlace = random_int (1, ($passwordLength-1));
+		//}
 		#loop that checks if it is the right place to add a number then checks if it's the right place to add a symbol and then adds a word
-		for($i =1; $i <$passwordLength; i++){
-			if($i == $numplace){
-				$pass = $pass + random_int(1,99);
-			}
-			if ($i == $symPlace){
-				$passw= $pass + char(random_int(33, 47))
-			}
-			$pass = $pass + $wordList[random_int(0, 10)];
-		}
-	}
+		//for($i =1; $i <$passwordLength; i++){
+		//	if($i == $numplace){
+		//		$pass = $pass + random_int(1,99);
+		//	}
+		//	if ($i == $symPlace){
+		//		$pass= $pass + char(random_int(33, 47))
+		//	}
+		//	$pass = $pass + $wordList[random_int(0, 10)];
+		//}
+	//}
 
 // Build an array with multiple words.  
 // Build a form that returns a string for the number of words
