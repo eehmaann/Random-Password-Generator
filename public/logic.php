6066c810f15e6b92515f35c8ef9f5d346f159e4e
Eric Ehmann <eaehmann@gmail.com>
2016-09-20 22:50:37 -0400

    hard coding a wrong result to test else statement

--- public/logic.php
+++ public/logic.php
@@ -12,15 +12,15 @@
 	'cook',
 	'minute',
 	'line'];
-	//$passwordLength =5;
-	//$passwordLength = (int)( $_POST["length"]);
-	//$wantNumber = ( $_POST["num"]);
-	//$wantSymbol = ( $_POST["sym"]);
-	//if($passwordLength >=1 && $passwordLength <=6){
+	$passwordLength =7;
+	$passwordLength = (int)( $_POST["length"]);
+	$wantNumber = ( $_POST["num"]);
+	$wantSymbol = ( $_POST["sym"]);
+	if($passwordLength >=1 && $passwordLength <=6){
 		#replace default string with one word
-	//	$pass = $wordList[rand(0, 10)];
+		$pass = $wordList[rand(0, 10)];
 		#If the user wants to use a number this will randomly select whaere it goes
-		//if($wantNumber == "Yes"){
+	//if($wantNumber == "Yes"){
 		//	$numPlace =rand(1, ($passwordLength-1));
 		//}
 		#If the user wants a symbol randomly choose where it should go
@@ -37,7 +37,7 @@
 			//}
 	//		$pass = $pass + $wordList[rand(0, 10)];
 	//	}
-	//}
-	//else{
-	//	$pass ="Please write the number of words in text box above";
-	//}
+	}
+	else{
+	$pass ="Please write the number of words in text box above";
+	}
